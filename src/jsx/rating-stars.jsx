58d2e87a45c45292c61ec38fab3d2afe1de73cb8
ratherblue <ratherblue@gmail.com>
2016-07-22 01:45:06 -0700

    tweak some responsive styles, add comments

--- src/jsx/rating-stars.jsx
+++ src/jsx/rating-stars.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
+// Don't have time to implement, but this would be a stand-alone module
+// that shows rating stars based on a numerator and a denominator.
+// Can use an icon font with `background-clip: text` to do partially-filled stars.
+
 export default React.createClass({
   render: function() {
     return (
       <span className="rating-stars">
         {this.props.rating}
-        <i className="fa fa-star" aria-hidden="true"></i>
-        <i className="fa fa-star" aria-hidden="true"></i>
-        <i className="fa fa-star" aria-hidden="true"></i>
-        <i className="fa fa-star" aria-hidden="true"></i>
       </span>
     );
   }
--- src/jsx/search-results.jsx
+++ src/jsx/search-results.jsx
@@ -2,14 +2,25 @@ import React from 'react';
 import MovieTile from './movie-tile';
 
 export default React.createClass({
+  renderHeading: function() {
+    if (this.props.results.length > 0) {
+      return (<h2>Results</h2>);
+    } else {
+      return;
+    }
+  },
   render: function() {
     var results = this.props.results.map(function(result) {
       return <MovieTile movie={result} />;
     });
+
     return (
-      <ul className="search-results">
-        {results}
-      </ul>
+      <div>
+        {this.renderHeading()}
+        <ul className="search-results">
+          {results}
+        </ul>
+      </div>
     );
   }
 });
