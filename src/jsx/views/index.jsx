e1578a3616e9f1252915f32de8bf0ce6b15ffc34
ratherblue <ratherblue@gmail.com>
2016-07-22 11:02:34 -0700

    added comments

--- src/jsx/views/index.jsx
+++ src/jsx/views/index.jsx
@@ -6,11 +6,11 @@ import SearchResults from '../components/search-results';
 
 
 require('../../scss/styles.scss'); // note: you could make component-specific
-                                // SCSS here, but just focusing on the
-                                // JavaScript due to time
+                                   // SCSS here, but just focusing on the
+                                   // JavaScript due to time
 
 const API_URL = 'http://api.themoviedb.org/3/search/movie'; // constants for these values since they don't change
-const API_KEY = '42b3e60b6636f50062f6d3579100d83f';
+const API_KEY = '42b3e60b6636f50062f6d3579100d83f'; // this could be an environment variable
 
 var MovieSearch = React.createClass({
   getInitialState: function() {
@@ -33,11 +33,14 @@ var MovieSearch = React.createClass({
     }
   },
   fetchData: function(page = 1) {
+    // probably better to have a helper function to construct the url
     var url = API_URL + '?api_key=' + API_KEY + '&query=' + this.state.searchTerm + '&page=' + page;
 
     fetch(url).then(function(response) {
-      return response.json();
+      return response.json(); // could add more error checking here
     }).then(function(response) {
+      // set state based on the results
+      // might need error checking here in a production app based on edge cases
       if (response.total_results > 0) {
         this.setState({
           totalResults: response.total_results,
@@ -49,10 +52,12 @@ var MovieSearch = React.createClass({
     }.bind(this));
   },
   previousPage: function() {
-    this.fetchData(this.state.page - 1);
+    this.fetchData(this.state.page - 1); // error checking to determine if we can page is on the component calling this
+                                         // probably better to move that logic here though
   },
   nextPage: function() {
-    this.fetchData(this.state.page + 1);
+    this.fetchData(this.state.page + 1); // error checking to determine if we can page is on the component calling this
+                                         // probably better to move that logic here though
   },
   handleSearchSubmit: function(e) {
     e.preventDefault(); // prevent form submission
